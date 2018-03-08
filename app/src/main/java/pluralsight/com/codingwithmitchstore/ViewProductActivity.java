0ae639d88ad7d8fbaf2c647826dfad242da9510e
Mitch <mitch@tabian.ca>
2018-03-07 18:43:32 -0800

    Module_2.4_End

--- app/src/main/java/pluralsight/com/codingwithmitchstore/ViewProductActivity.java
+++ app/src/main/java/pluralsight/com/codingwithmitchstore/ViewProductActivity.java
@@ -3,11 +3,17 @@ package pluralsight.com.codingwithmitchstore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 import pluralsight.com.codingwithmitchstore.models.Product;
+import pluralsight.com.codingwithmitchstore.resources.Products;
 
 
 /**
@@ -19,16 +25,22 @@ public class ViewProductActivity extends AppCompatActivity{
     private static final String TAG = "ViewProductActivity";
 
     //widgets
+    private ViewPager mProductContainer;
+    private TabLayout mTabLayout;
 
     //vars
     private Product mProduct;
+    private ProductPagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product);
+        mProductContainer = findViewById(R.id.product_container);
+        mTabLayout = findViewById(R.id.tab_layout);
 
         getIncomingIntent();
+        initPagerAdapter();
     }
 
     private void getIncomingIntent(){
@@ -38,9 +50,34 @@ public class ViewProductActivity extends AppCompatActivity{
         }
     }
 
+    private void initPagerAdapter(){
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        Products products = new Products();
+        Product[] selectedProducts = products.PRODUCT_MAP.get(mProduct.getType());
+        for(Product product: selectedProducts){
+            Bundle bundle = new Bundle();
+            bundle.putParcelable(getString(R.string.intent_product), product);
+            ViewProductFragment viewProductFragment = new ViewProductFragment();
+            viewProductFragment.setArguments(bundle);
+            fragments.add(viewProductFragment);
+        }
+        mPagerAdapter = new ProductPagerAdapter(getSupportFragmentManager(), fragments);
+        mProductContainer.setAdapter(mPagerAdapter);
+        mTabLayout.setupWithViewPager(mProductContainer, true);
+    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
