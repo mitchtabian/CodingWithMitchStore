0e6716c4ef6bf20d73552633d0fb4302a2c06176
Mitch <mitch@tabian.ca>
2018-03-20 15:34:50 -0700

    update

--- app/src/main/java/pluralsight/com/codingwithmitchstore/customviews/ScalingImageView.java
+++ app/src/main/java/pluralsight/com/codingwithmitchstore/customviews/ScalingImageView.java
@@ -3,12 +3,13 @@ package pluralsight.com.codingwithmitchstore.customviews;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-
+import android.view.View;
+import android.widget.ImageView;
 /**
  * Created by User on 3/9/2018.
  */
 
-public class ScalingImageView extends android.support.v7.widget.AppCompatImageView {
+public class ScalingImageView extends ImageView{
 
     private static final String TAG = "ScalingImageView";
 
